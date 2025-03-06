a1e609863ed379391ba43b0c41e41385ca77eb3e
alejandra206 <alejandralopez0953@gmail.com>
2025-03-05 21:37:27 -0600

    Creacion de subclase Deportista-hecho por alejandra lopez.

---
+++ Persona/src/persona/Deportista.java
@@ -0,0 +1,26 @@
+package persona;
+
+class Deportista extends Persona {
+    private String deporte;
+    
+    public Deportista(String nombre, int edad, double dni, String deporte) {
+        super(nombre, edad, dni);
+        this.deporte = deporte;
+    }
+    
+    public String getDeporte() {
+        return deporte;
+    }
+    
+    public void setDeporte(String deporte) {
+        this.deporte = deporte;
+    }
+    
+    public void mostrarInformacion() {
+        System.out.println("Deportista: " + nombre);
+        System.out.println("Edad: " + edad);
+        System.out.println("DNI: " + dni);
+        System.out.println("Deporte: " + deporte);
+    
+    }
+}
